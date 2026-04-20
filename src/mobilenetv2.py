af0f2998da2054a54c8362cbee4941f9a1045bb9
weinkyls <miaoxn.chong@gmail.com>
2026-04-20 15:13:55 +0100

    removing old files

--- src/mobilenetv2.py
+++
@@ -1,8 +0,0 @@
-from rknlite.api import RKNNLite
-
-rknn_lite = RKNNLite()
-rknn_lite.load_rknn('./yolov8n-rk3588.rknn')
-rknn_lite.init_runtime()
-
-outputs = rknn_lite.inference(inputs=[input_data])
-rknn_lite.release()
